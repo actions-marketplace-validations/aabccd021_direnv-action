b1a218aa6908ba72727227301041dc437034c6d6
aabccd021 <aabccd021@gmail.com>
2022-10-31 19:43:40 +0700

    fix: remove direnv allow

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cp = require("child_process");
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    cp.execSync('direnv allow', { encoding: "utf-8" });
     const envs = JSON.parse(cp.execSync('direnv export json', { encoding: "utf-8" }));
 
     Object.keys(envs).forEach(function (name) {
